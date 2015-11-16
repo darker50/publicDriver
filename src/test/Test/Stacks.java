1389ad6b40f3858f76dcda6f58e027f7f4ae8846
Nelson <236066480@qq.com>
2015-11-16 16:16:35 +0800

    fix a bug

--- src/test/Test/Stacks.java
+++ src/test/Test/Stacks.java
@@ -8,17 +8,13 @@ public class Stacks {
 	public static void main(String[] args){
 		Stack stk  = new Stack();
 		for (int i = 0; i < months.length; i++) 
-//			System.out.println(months[i]);
 			stk.push(months[i] + "");
 		System.out.println("stk = " + stk);
-		
 		stk.addElement("The last line");
-//		System.out.println("element 5 =" + stk.elementAt(5));
+		System.out.println("element 5 =" + stk.elementAt(5));
 		System.out.println("popping elements:");
 		while (!stk.empty()) {
 			System.out.println(stk.pop());
-			
-		
 		}
 	}
 }
